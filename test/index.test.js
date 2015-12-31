c9f090f2b516b50960e47dc339e1a3b75e52370d
Anita <a.nita@hotmail.co.uk>
2015-12-31 14:24:15 +0000

    initialize hapi server

---
+++ test/index.test.js
@@ -0,0 +1,21 @@
+require('env2')('.env');
+var Code = require('code');
+var Lab = require('lab');
+var Hapi = require('hapi');
+var Server = require('../lib/index');
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var it = lab.test;
+
+it('starts server and return an instance of Hapi server', function (done) {
+
+  Server.init(0, function (err, server) {
+
+    expect(err).to.not.exist();
+    expect(server).to.be.instanceof(Hapi.Server);
+
+    server.stop(done);
+  });
+
+});
