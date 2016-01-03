f02f29fcf044d7f2e07be4f84d6de20751a1844e
Anita <a.nita@hotmail.co.uk>
2016-01-03 18:46:46 +0000

    new endpoints :admin, blog, create , init db - es

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('test');
+(function () {
+
+	document.getElementById('post').addEventListener('click', function () {
+
+		document.getElementById('create-post').style.visibility = 'visible';
+
+	}, false); 
+
+})();
